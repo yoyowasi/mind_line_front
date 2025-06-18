559db1cc508fc9b04233bba749029cb0f10c6428
yoyowasi <youmin302@gmail.com>
2025-06-18 19:25:29 +0900

    일단 기본 레이아웃 완료 올려잇!!!

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -3,8 +3,11 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+    id("com.google.gms.google-services")
 }
-
+dependencies {
+    implementation(platform("com.google.firebase:firebase-bom:33.15.0"))
+}
 android {
     namespace = "com.daelim.haru_emtion_front"
     compileSdk = flutter.compileSdkVersion
